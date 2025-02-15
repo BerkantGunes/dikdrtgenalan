ea6c5b2470ea213f64799a655096d95c886eff36
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-15 09:17:08 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dikdrtgenalan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kisa = Convert.ToInt32(txtKisa.Text);
+            int uzun = Convert.ToInt32(txtUzun.Text);
+            int alan = kisa * uzun;
+            int cevre = 2 * (kisa + uzun);
+            label3.Text = "Cevre: " + cevre;
+            label4.Text = "Alan: " + alan;
+        }
+    }
+}
